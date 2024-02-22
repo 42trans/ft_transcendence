ffb7f4f78886acf279c17f18dbaebe71926627b0
hioikawa <uminomae.com>
2024-02-22 11:24:06 +0900

    📕test.sh: コメント整理

--- test/django/sample_django.sh
+++ test/django/sample_django.sh
@@ -3,18 +3,18 @@
 # -o /dev/null: curlの出力を/dev/nullに
 # -w "%{http_code}\n": HTTPレスポンスコードを出力　※それを変数に
 # -k: 証明書skip
+echo "test GET https://localhost"
 RESPONSE_CODE=$(curl -o /dev/null -s  -w "%{http_code}\n" -k https://localhost)
-
 if [ "$RESPONSE_CODE" = "200" ]; then
-    echo "Django ok"
+    echo "200 ok"
 else
-    echo "Django ng. Response code: $RESPONSE_CODE"
+    echo "ng. Response code: $RESPONSE_CODE"
 fi
 
+echo -e "\\ntest GET https://hioikawa.42.fr"
 RESPONSE_CODE=$(curl -o /dev/null -s  -w "%{http_code}\n" -k https://hioikawa.42.fr)
-
 if [ "$RESPONSE_CODE" = "200" ]; then
-    echo "Django ok"
+    echo "200 ok"
 else
-    echo "Django ng. Response code: $RESPONSE_CODE"
+    echo "ng. Response code: $RESPONSE_CODE"
 fi
