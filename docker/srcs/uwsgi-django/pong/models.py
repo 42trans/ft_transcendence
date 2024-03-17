05f3cbe395b78fbf85925fd76270381fcd65b69e
hioikawa <uminomae.com>
2024-03-17 20:17:51 +0900

    DBの補足

--- docker/srcs/uwsgi-django/pong/models.py
+++ docker/srcs/uwsgi-django/pong/models.py
@@ -1,6 +1,7 @@
 # docker/srcs/uwsgi-django/pong/models.py
 from django.db import models
 
+# DBのテーブルはこれに行を足してもらえば、あとはDjangoがやります（サンプル動作してます）。
 class PongGameResult(models.Model):
     # フィールドの定義
     match_id = models.IntegerField()
@@ -10,3 +11,7 @@ class PongGameResult(models.Model):
     name_loser = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
 
+# あとはこんな感じの呼び出しで、とあるURL（第一引数）にきたPOSTは、指定の関数（第二引数、処理内容はDBに保存する）が動く
+# path('api/save_game_result/', views.save_game_result, name='save_game_result'),
+# この関数自体は一関数25行程度なので、気楽に増やしちゃっていいと思います。
+# 必要性がよくわからない時は、とりあえずデータ入れちゃって、取り出す時に「選ぶ」とかで良いのかもしれない。
