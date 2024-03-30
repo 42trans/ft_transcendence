25af56473f2961326e442a7e59568b90ec519c76
hioikawa <uminomae.com>
2024-03-30 21:56:47 +0900

    📢アラートの種類に応じて通知先振り分け:labelsがpriority = "high"でSlackに通知 ※grafana-dev/はplay用です。削除予定。 - 通知ポリシーツリーの設定 - アラートにlabelsを設定

--- test/grafana/test_main_grafana.sh
+++ test/grafana/test_main_grafana.sh
@@ -27,9 +27,9 @@ echo -e "------------------------------------------------------"
 echo -e ".env: GRAFANA_PORT=${GRAFANA_PORT} "
 echo -e "3032 ならOK"
 my_func_cmp "$GRAFANA_PORT" "3032"
-echo -e "------------------------------------------------------"
-echo -e "stats(統計情報)を表示"
-curl -H "Accept: application/json" -H "Content-Type: application/json" http://adm:adm@localhost:${GRAFANA_PORT}/api/admin/stats
+# echo -e "------------------------------------------------------"
+# echo -e "stats(統計情報)を表示"
+# curl -H "Accept: application/json" -H "Content-Type: application/json" http://adm:adm@localhost:${GRAFANA_PORT}/api/admin/stats
 
 #=======================================================
 echo "${ESC}${COLOR201}"
@@ -76,14 +76,14 @@ my_func_cmp "$GET_VALUE" "10_4_0"
 #=======================================================
 echo "${ESC}${COLOR201}"
 echo -e "------------------------------------------------------"
-echo -e " 目的: プロビジョニングは設定されているか？"
+echo -e " 目的: dashboardは設定されているか？"
 echo -e " 内容: フォルダー/ダッシュボード検索一覧を取得"
 echo -e " [cmd]: curl http://adm:adm@localhost:3032/api/search"
 # 参考:【フォルダー/ダッシュボード検索 HTTP API | Grafana のドキュメント】 <https://grafana.com/docs/grafana/latest/developers/http_api/folder_dashboard_search/>
 #=======================================================
 echo -e "------------------------------------------------------"
 GET_OUTPUT=$(curl -s -S http://adm:adm@localhost:3032/api/search)
-echo -e "$GET_OUTPUT"
+# echo -e "$GET_OUTPUT"
 # $GET_OUTPUT expected:
 # [{"id":1,"uid":"rYdddlPWk","title":"Node Exporter Full","uri":"db/node-exporter-full","url":"/d/rYdddlPWk/node-exporter-full","slug":"","type":"dash-db","":["linux"],"isStarred":false,"sortMeta":0}]
 # 配列になっているので注意！
@@ -91,3 +91,37 @@ GET_VALUE=$(echo "$GET_OUTPUT" | jq -r '.[1].title')
 echo -e "jq -r '.title': $GET_VALUE"
 echo -e "Node Exporter Full ならOK"
 my_func_cmp "$GET_VALUE" "Node Exporter Full"
+
+
+#=======================================================
+echo "${ESC}${COLOR201}"
+echo -e "------------------------------------------------------"
+echo -e " 目的: 通知ポリシーは設定されているか？"
+echo -e " 内容: 通知ポリシーの一覧を取得"
+echo -e " [cmd]: curl -X GET -s -S -u "adm:adm" http://localhost:${GRAFANA_PORT}/api/v1/provisioning/policies"
+# 参考:【HTTP API を使用してアラート リソースを管理する | Grafana のドキュメント】 <https://grafana.com/docs/grafana/latest/alerting/set-up/provision-alerting-resources/http-api-provisioning/>
+#=======================================================
+echo -e "------------------------------------------------------"
+GET_OUTPUT=$(curl -X GET -s -S -u "adm:adm" http://localhost:${GRAFANA_PORT}/api/v1/provisioning/policies)
+# echo -e "$GET_OUTPUT"
+# $GET_OUTPUT expected:
+# {"receiver":"hioikawa-Discord","group_by":["grafana_folder","alertname"],"routes":[{"receiver":"hioikawa-Slack","group_by":["grafana_folder","alertname"],"matchers":["priority=\"high\""],"group_wait":"30s","group_interval":"5m","repeat_interval":"4h"}],"provenance":"file"}
+GET_VALUE=$(echo "$GET_OUTPUT" | jq -r '.receiver')
+echo -e "jq -r '.receiver': $GET_VALUE"
+echo -e "hioikawa-Discord ならOK"
+my_func_cmp "$GET_VALUE" "hioikawa-Discord"
+
+echo "${ESC}${COLOR201}"
+GET_VALUE=$(echo "$GET_OUTPUT" | jq -r '.routes[0].receiver')
+echo -e "jq -r '.routes[0].receiver': $GET_VALUE"
+echo -e "hioikawa-Slack ならOK"
+my_func_cmp "$GET_VALUE" "hioikawa-Slack"
+#=======================================================
+# echo -e "\n"
+# echo -e "alert表示"
+# curl -X GET -u "adm:adm" http://localhost:${GRAFANA_PORT}/api/v1/provisioning/alert-rules
+#=======================================================
+# echo -e "\n"
+# echo -e "contact-points表示"
+# curl -X GET -u "adm:adm" http://localhost:${GRAFANA_PORT}/api/v1/provisioning/contact-points
+#=======================================================
