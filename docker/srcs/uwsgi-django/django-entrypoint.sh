0461d73d9195f80cb3e996b3ac58e01c99a43494
takira <ca.fie.0220@gmail.com>
2024-04-07 17:38:12 +0900

    [add] add initial user; user1 and user2

--- docker/srcs/uwsgi-django/django-entrypoint.sh
+++ docker/srcs/uwsgi-django/django-entrypoint.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 # docker/srcs/uwsgi-django/django-entrypoint.sh
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+# PostgreSQLサーバーが起動するまで待機
+i=30
+while [ $i -gt 0 ]; do
+    if pg_isready -h postgres -p 5432; then
+        break
+    fi
+    echo "Waiting for PostgreSQL to become available..."
+    sleep 1
+    i=$(( i - 1 ))
+done
+if [ $i -eq 0 ]; then
+    echo >&2 'PostgreSQL did not start'
+    exit 1
+fi
+echo "PostgreSQL is available. Continuing with database migrations."
+
+
 python manage.py migrate --noinput
-# superuserの作成 データベースに格納される。作成に失敗しても進む。
-# uwsgi-django/.envを参照
-python manage.py createsuperuser --no-input || true 
 
 # register superuser
 python manage.py create_superuser
 
+# register init user
+python /code/trans_pj/add_test_user.py
 
 # 全部の"static/"から、ルートのstatic/に集める
 python manage.py collectstatic --noinput
