23010a585e72c7b90aa7d584cc9416667079a1b5
takira <ca.fie.0220@gmail.com>
2024-04-10 13:10:33 +0900

    [add] test 2fa: enable/verify 2fa form

--- docker/srcs/uwsgi-django/accounts/tests/test_2fa.py
+++ docker/srcs/uwsgi-django/accounts/tests/test_2fa.py
@@ -3,8 +3,124 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.messages import get_messages
 from django.urls import reverse, resolve
 from django.test import TestCase
-from accounts.forms import UserEditForm
-from accounts.views.user import EditUserProfileView
+from accounts.forms import Enable2FAForm, Verify2FAForm
+from accounts.views.two_factor_auth import Enable2FaView, Verify2FaView
+
+
+class Enable2FaFormTests(TestCase):
+    kUserEmail = 'test@example.com'
+    kUserNickname = 'test'
+    kUserPassword = 'pass012345'
+
+    kLoginName = "accounts:login"
+    kEnable2FaName = "accounts:enable_2fa"
+    kEnable2FaURL = "/accounts/verify/enable_2fa/"
+
+    def setUp(self):
+
+        User = get_user_model()
+        user = User.objects.create_user(email=self.kUserEmail,
+                                        nickname=self.kUserNickname,
+                                        password=self.kUserPassword)
+        user.save()
+
+        login_url = reverse(self.kLoginName)
+        user_field = {
+            'username'  : self.kUserEmail,
+            'password'  : self.kUserPassword,
+        }
+        self.response = self.client.post(login_url, user_field)
+
+        # access to enable_2fa
+        enable_2fa_url = reverse(self.kEnable2FaName)
+        self.response = self.client.get(enable_2fa_url)
+
+    def test_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_url_resolve_view(self):
+        enable_2fa_view = resolve(self.kEnable2FaURL)
+        self.assertEqual(enable_2fa_view.func.view_class, Enable2FaView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, Enable2FAForm)
+
+    def test_form_inputs(self):
+        '''
+        The edit form should contain 5 <input> tags:
+         (csrf), token
+        '''
+        self.assertContains(self.response, '<input', 2)
+        self.assertContains(self.response, 'type="hidden"', 1)  # csrf
+        self.assertContains(self.response, 'type="text"', 1)    # token
+
+    def test_input_invalid_token(self):
+        invalid_tokens = [
+            '', 'a', '@', '.',
+            'abcdef', '123abc'
+            '000000', '999999',
+            '+000000', '-00000', '0000.0',
+            '000000a', 'aaaaaa',
+            '00000000', '000000-0000',
+            ' 000000 ',
+            '０００００００',
+        ]
+
+        for token in invalid_tokens:
+            form = Enable2FAForm(data={'token': token})
+            self.assertFalse(form.is_valid())
+
+
+class Verify2FaFormTests(TestCase):
+    kUserEmail = 'test@example.com'
+    kUserNickname = 'test'
+    kUserPassword = 'pass012345'
+
+    kLoginName = "accounts:login"
+    kVerify2FaURL = "/accounts/verify/verify_2fa/"
+
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user(email=self.kUserEmail,
+                                        nickname=self.kUserNickname,
+                                        password=self.kUserPassword,
+                                        enable_2fa=True)
+        user.save()
+
+        login_url = reverse(self.kLoginName)
+        user_field = {
+            'username'  : self.kUserEmail,
+            'password'  : self.kUserPassword,
+        }
+        self.response = self.client.post(login_url, user_field, follow=True)
+        # login -> redirect to verify_2fa
+
+    def test_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_url_resolve_view(self):
+        enable_2fa_view = resolve(self.kVerify2FaURL)
+        self.assertEqual(enable_2fa_view.func.view_class, Verify2FaView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, Verify2FAForm)
+
+    def test_form_inputs(self):
+        '''
+        The edit form should contain 5 <input> tags:
+         (csrf), token
+        '''
+        self.assertContains(self.response, '<input', 2)
+        self.assertContains(self.response, 'type="hidden"', 1)  # csrf
+        self.assertContains(self.response, 'type="text"', 1)    # token
 
 
 class NotLoginUserAccessEnable2FaTests(TestCase):
