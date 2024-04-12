96b57c8f7a3bd76b690870bf42c17d49c632d09e
hioikawa <uminomae.com>
2024-04-12 10:53:14 +0900

    コメント:使い方

--- docker/srcs/webpack2/ft_trans/src/index.ts
+++ docker/srcs/webpack2/ft_trans/src/index.ts
@@ -9,7 +9,17 @@
  * - public/へのビルド方法: docker exec -it webpack2 bash -c "npm run build"
  * - buildにより、一つのjsファイル（public/bundle.js）にまとめて生成されます。　
  * - そのファイルと合わせて、public/内のファイルをすべて、django/static/share_webpack2などのディレクトリに保存してください。共有ボリュームを設定しているので、再起動時二回以内にマウントされる予定ですが、不安定な時は手動で行ってください。
+ * 
+ * ## 使い方
+ * - ホスト開発用サーバーの起動 ※hotReload機能
+ *   - cd docker/srcs/webpack2/ft_trans && npm run dev
+ *   - コンテナの8181はwsが機能してません。が、一応動きます。けど、使わないよう。
+ * - Django　static用のpublic
+ *   - docker exec -it webpack2 bash -c "npm run build"
+ *   - webpack2/public/に一式できます。マウント共有してるので二回再起動すればDjangoに認識されるはず
+ *   - static/share_wabpack2/に自動で入ります。ホストマシンに保存されます。.gitignore済み
  */
+
 import App from './js/App'
 
 App.main();
