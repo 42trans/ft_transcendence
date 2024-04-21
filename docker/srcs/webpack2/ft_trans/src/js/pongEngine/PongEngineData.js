1153b962f01a1a4a0c05bacd4084f91333f1f6a6
hioikawa <uminomae.com>
2024-04-21 17:17:03 +0900

    [rfct]pongEngine/

--- docker/srcs/webpack2/ft_trans/src/js/pongEngine/PongEngineData.js
+++ docker/srcs/webpack2/ft_trans/src/js/pongEngine/PongEngineData.js
@@ -1,40 +1,30 @@
 import PongEngine from "./PongEngine";
 import PongEngineInit from "./PongEngineInit";
 
-class PongEngineData {
-	constructor(engine) {
-		this.engine = engine;
-		this.config = engine.config;
-		this.scene = engine.scene;
+class PongEngineData 
+{
+	constructor(pongEngine) 
+	{
+		this.pongEngine = pongEngine;
+		this.config = pongEngine.config;
+		this.scene = pongEngine.scene;
 		this.objects = {};
-		this.settings = {
-			maxScore: this.config.gameSettings.MAX_SCORE,
-			initBallSpeed: this.config.gameSettings.INIT_BALL_SPEED,
-			maxBallSpeed: this.config.gameSettings.MAX_BALL_SPEED,
-			difficulty: this.config.gameSettings.DIFFICULTY,
-			field: {
-				width: this.config.fields.WIDTH,
-				height: this.config.fields.HEIGHT
-			}
-		}
-		this.state = {
+		this.settings = {};
+		this.state = 
+		{
 			score1: 0,
 			score2: 0,
 		};
 	}
 
-	initObjects() {
-		const initializer = new PongEngineInit(this.scene, this.config);
-		this.objects = initializer.createGameObjects();
-		Object.keys(this.objects).forEach(key => {
-			this.scene.add(this.objects[key]);
-		});
-	}
-
-	updateScore(player, score) {
-		if (player === 1) {
+	updateScore(player, score) 
+	{
+		if (player === 1) 
+		{
 			this.state.score1 += score;
-		} else if (player === 2) {
+		} 
+		else if (player === 2) 
+		{
 			this.state.score2 += score;
 		}
 	}
--- docker/srcs/webpack2/ft_trans/src/js/pongEngine/PongEnginePhysics.js
+++ docker/srcs/webpack2/ft_trans/src/js/pongEngine/PongEnginePhysics.js
@@ -1,24 +1,31 @@
 /**
  * 物理演算に関する処理を担当。壁やパドルとの衝突判定、ボールの方向や速度の調整を行う。
  */
-class PongEnginePhysics {
-	constructor(pongEngineData) {
+class PongEnginePhysics 
+{
+	constructor(pongEngineData) 
+	{
 		this.field = pongEngineData.settings.field;
 		this.maxBallSpeed = pongEngineData.settings.maxBallSpeed;
 	}
 
-	// 左右の壁との衝突しているか？
-	isCollidingWithSideWalls(ballX, r, field) {
+	// 左右の壁と衝突しているか？
+	isCollidingWithSideWalls(ballX, r, field) 
+	{
 		return	ballX - r <= -field.width / 2 || 
-				ballX + r >= field.width / 2
+		ballX + r >= field.width / 2
 	}
-
-	isCollidingWithCeilingOrFloor(ballY, r, field) {
+	
+	// 上下の壁と衝突しているか？
+	isCollidingWithCeilingOrFloor(ballY, r, field) 
+	{
 		return	ballY - r <= -field.height / 2 || 
-				ballY + r >= field.height / 2
+		ballY + r >= field.height / 2
 	}
-
-	isBallCollidingWithPaddle(ball, paddle) {
+	
+	// パドルとボールが衝突しているか？
+	isBallCollidingWithPaddle(ball, paddle) 
+	{
 		const r			= ball.geometry.parameters.radius;
 		const ballX		= ball.position.x;
 		const ballY		= ball.position.y;
@@ -31,14 +38,15 @@ class PongEnginePhysics {
 				ballY - r <= paddleY + paddle.height / 2
 	}
 
-	adjustBallDirectionAndSpeed(ball, paddle) {
+	adjustBallDirectionAndSpeed(ball, paddle) 
+	{
 		const ballX = ball.position.x;
 		const paddleX = paddle.position.x;
 
 		// ボールがパドルに向かって進んでいるかどうか（X方向の判定）
 		if (  (ball.dirX < 0 && ballX > paddleX) 
-			||(ball.dirX > 0 && ballX < paddleX)
-		) {
+			||(ball.dirX > 0 && ballX < paddleX)) 
+		{
 			ball.dirX	= -ball.dirX;  // ボールの水平方向を反転
 			ball.dirY	+= paddle.dirY * 0.05;  // パドルの移動方向がボールに影響
 			ball.speed	= Math.min(ball.speed * 1.1, this.maxBallSpeed);  // 速度を最大速度を超えないように10%増加
