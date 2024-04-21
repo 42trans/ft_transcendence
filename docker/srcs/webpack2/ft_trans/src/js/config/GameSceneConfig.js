a1e2d31e680d5f4bd69e693726b72fab4721250c
hioikawa <uminomae.com>
2024-04-21 17:06:56 +0900

    [rfct]config

--- docker/srcs/webpack2/ft_trans/src/js/config/GameSceneConfig.js
+++ docker/srcs/webpack2/ft_trans/src/js/config/GameSceneConfig.js
@@ -8,13 +8,14 @@
 import * as THREE from 'three';
 import BaseSceneConfig from './BaseSceneConfig.js'
 
-class GameSceneConfig extends BaseSceneConfig{
-
-	constructor() {
+class GameSceneConfig extends BaseSceneConfig
+{
+	constructor() 
+	{
 		super();
 		
-		/** @type {{ fov: number, aspect: number, near: number, far: number, position: THREE.Vector3, lookAt: THREE.Vector3 }} */
-		this.cameraConfig = {
+		this.cameraConfig = 
+		{
 			...this.cameraConfig,
 			fov: 50,
 			far: 1500,
@@ -22,8 +23,8 @@ class GameSceneConfig extends BaseSceneConfig{
 			up: new THREE.Vector3(0, 0, 1)  // Z軸が上方向（真下を向いてるので）
 		};
 
-		/** @type {{ enableDamping: boolean, dampingFactor: number, screenSpacePanning: boolean, maxPolarAngle: number, minDistance: number, maxDistance: number, rotateSpeed: number, zoomSpeed: number, autoRotate: boolean, autoRotateSpeed: number }} */
-		this.controlsConfig = {
+		this.controlsConfig = 
+		{
 			...this.controlsConfig,
 
 			minPolarAngle: 0,  // 0 = カメラが真上から見下ろす
@@ -33,17 +34,11 @@ class GameSceneConfig extends BaseSceneConfig{
 			
 			minDistance: 300,
 			maxDistance: 1000,
-			// rotateSpeed: 1.0,
 			zoomSpeed: 1.2,
-			// autoRotate: false,
 			enableZoom: true,
-			enablePan: true,
-			enableRotate: true,
-			// autoRotateSpeed: 1.0,
 			dampingFactor: 0.25,
 		};
 
-		/** @type {Array<{type: string, color: number, intensity: number, position?: THREE.Vector3, name: string}>} */
 		this.lightsConfig = [
 			{
 				type: 'AmbientLight',
@@ -53,7 +48,6 @@ class GameSceneConfig extends BaseSceneConfig{
 			},
 			{
 				type: 'DirectionalLight',
-				// color: 0xa41832,
 				color: this.rgbToHex(20, 200, 5),
 				intensity: 1,
 				position: new THREE.Vector3(-50, 50, -50),
@@ -70,7 +64,6 @@ class GameSceneConfig extends BaseSceneConfig{
 			{
 				type: 'PointLight',
 				color: new THREE.Color(200, 100, 100),
-				// color: 0x4b27ce,
 				intensity: 0.5,
 				distance: 300,
 				decay: 1,
@@ -90,29 +83,12 @@ class GameSceneConfig extends BaseSceneConfig{
 			},
 		];
 
-		/** @type {Array<{path: string, initialPosition: THREE.Vector3, initialScale: THREE.Vector3, initialRotation: THREE.Euler, name: string, defaultAnimation: string, textures: {baseColor: string, normalMap: string, specularMap: string}}>} */
 		this.modelsConfig = [
-			// {
-			// 	path: '../assets/vespa_mandarinia/scene.gltf',
-			// 	initialPosition: new THREE.Vector3(40, 40, -50),
-			// 	initialScale: new THREE.Vector3(10, 10, 10),
-			// 	name: 'suzumebachi',
-			// 	defaultAnimation: 'Hover',
-			// },
 		];
 
-		/** @type {Array<{model: string, autoplay: boolean}>} */
 		this.animationsConfig = [
-			// {
-			// 	model: 'suzumebachi',
-			// 	// autoplay: true
-			// },
 		];
 
-	}
-
-	rgbToHex(r, g, b) {
-		return (r << 16) + (g << 8) + b;
 	}
 }
 
--- docker/srcs/webpack2/ft_trans/src/js/config/RendererConfig.js
+++ docker/srcs/webpack2/ft_trans/src/js/config/RendererConfig.js
@@ -2,8 +2,11 @@
  * @file シーンの初期値を指定するconfigファイル
  * - 参考:【WebGLRenderer – three.js docs】 <https://threejs.org/docs/#api/en/renderers/WebGLRenderer>
  */
-class RendererConfig {
-	constructor() {
+class RendererConfig 
+{
+	constructor() 
+	{
+		// TODO_ft:　<div>から<canvas>への挿入
 		// const canvas = document.getElementById('threejs-canvas-container');
 		/** @type {{ antialias: boolean, pixelRatio: number, alpha: boolean }} */
 		this.rendererOptions = {
