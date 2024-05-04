c7f8ddc670a2026e77d4e6beeefeb3e400bfe1e8
takira <ca.fie.0220@gmail.com>
2024-05-04 11:41:32 +0900

    [refactor] chat/views.py: FBV -> CBV

--- docker/srcs/uwsgi-django/chat/urls.py
+++ docker/srcs/uwsgi-django/chat/urls.py
@@ -1,12 +1,12 @@
 # chat/urls.py
 
 from django.urls import path, re_path
-from chat.views.views import dm_list, dm_with
+from chat.views.views import DMList, DMView
 
 
 app_name = 'chat'
 
 urlpatterns = [
-    path('dm-list/', dm_list, name='dm_list'),
-    path('dm-with/<str:nickname>/', dm_with, name='dm'),
+    path('dm-list/', DMList.as_view(), name='dm_list'),
+    path('dm-with/<str:nickname>/', DMView.as_view(), name='dm'),
 ]
--- docker/srcs/uwsgi-django/chat/views/views.py
+++ docker/srcs/uwsgi-django/chat/views/views.py
@@ -1,69 +1,74 @@
 # chat/views.py
 
+import logging
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
-from rest_framework.views import APIView
+from django.views.generic import TemplateView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
 
 from accounts.models import CustomUser
 from chat.models import DMSession, Message
-
-import logging
 
 
 logging.basicConfig(
     level=logging.ERROR,
     format='%(asctime)s - %(levelname)s - %(message)s - [in %(funcName)s: %(lineno)d]',
 )
+
 logger = logging.getLogger(__name__)
 
 
-def print_blue(text):
-    print(f"\033[34m[DEBUG] {text}\033[0m")
+class DMView(TemplateView):
+    template_name = "chat/dm.html"
+    error_occurred_redirect_to = "chat:list"
+    not_authenticated_redirect_to = "accounts:login"
+
+    def get(self, request, nickname):
+        if not request.user.is_authenticated:
+            return redirect(self.not_authenticated_redirect_to)
+
+        user = request.user
+        if nickname == user.nickname:
+            messages.error(request, "You cannot send a message to yourself")
+            return redirect(self.error_occurred_redirect_to)
+
+        try:
+            target_user = CustomUser.objects.get(nickname=nickname)
+        except CustomUser.DoesNotExist:
+            messages.error(request, "The specified user does not exist")
+            return redirect(self.error_occurred_redirect_to)
+
+        # DMからログを取得
+        other_user = CustomUser.objects.get(id=target_user.id)
+        message_log = Message.objects.filter(
+            sender__in=[user, other_user],
+            receiver__in=[user, other_user]
+        ).order_by('timestamp')
+
+        is_blocking_user = user.blocking_users.filter(id=target_user.id).exists()
+
+        data = {
+            'nickname'      : other_user.nickname,
+            'messages'      : message_log,
+            'isBlockingUser': is_blocking_user,
+            'isSystemUser'  : other_user.is_system
+        }
+        # logging.error(f'dm_room: user: {user.nickname}, dm_to: {nickname}, blocking: {is_blocking_user}')
+        return render(request, self.template_name, data)
 
 
-# todo: CBV API
-def dm_with(request, nickname):
-    if not request.user.is_authenticated:
-        return redirect("accounts:login")
 
-    user = request.user
-    if nickname == user.nickname:
-        messages.error(request, "You cannot send a message to yourself")
-        return redirect('chat:dm_list')
+class DMList(TemplateView):
+    template_name = "chat/dm_list.html"
+    not_authenticated_redirect_to = "accounts:login"
 
-    try:
-        target_user = CustomUser.objects.get(nickname=nickname)
-    except CustomUser.DoesNotExist:
-        messages.error(request, "The specified user does not exist")
-        return redirect('chat:dm_list')
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect(self.not_authenticated_redirect_to)
 
-    # DMからログを取得
-    other_user = CustomUser.objects.get(id=target_user.id)
-    message_log = Message.objects.filter(
-        sender__in=[user, other_user],
-        receiver__in=[user, other_user]
-    ).order_by('timestamp')
-
-    is_blocking_user = user.blocking_users.filter(id=target_user.id).exists()
-
-    data = {
-        'nickname': other_user.nickname,
-        'messages': message_log,
-        'isBlockingUser': is_blocking_user,
-        'isSystemUser': other_user.is_system
-    }
-    logging.error(f'dm_room: user: {user.nickname}, dm_to: {nickname}, blocking: {is_blocking_user}')
-    return render(request, 'chat/dm.html', data)
-
-
-
-# todo: CBV API
-def dm_list(request):
-    if not request.user.is_authenticated:
-        return redirect("accounts:login")
-    return render(request, "chat/dm_list.html")
+        return render(request, self.template_name)
