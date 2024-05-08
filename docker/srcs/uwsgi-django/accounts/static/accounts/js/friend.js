30f9cebed3574fecc00f0a26656b65ec1362eba7
takira <ca.fie.0220@gmail.com>
2024-05-08 17:14:29 +0900

    [update] friend削除の際にconfirmを追加

--- docker/srcs/uwsgi-django/accounts/static/accounts/js/friend.js
+++ docker/srcs/uwsgi-django/accounts/static/accounts/js/friend.js
@@ -82,54 +82,63 @@ function acceptFriendRequest(userId) {
 
 
 function rejectFriendRequest(userId) {
-    fetch(`/accounts/api/friend/reject-request/${userId}/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-        }
-    })
-        .then(response => {
-            return response.json().then(data => {
-                if (!response.ok) {
-                    throw new Error(data.error);
-                }
-                return data;
+    if (confirm('Are you sure you want to reject this request ?')) {
+        fetch(`/accounts/api/friend/reject-request/${userId}/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+            .then(response => {
+                return response.json().then(data => {
+                    if (!response.ok) {
+                        throw new Error(data.error);
+                    }
+                    return data;
+                });
+            })
+            .then(data => {
+                alert(data.status);
+                window.location.reload();  // ページをリロード
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert(error.message);
             });
-        })
-        .then(data => {
-            alert(data.status);
-            window.location.reload();  // ページをリロード
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert(error.message);
-        });
+    } else {
+        alert('Request rejection has been canceled');
+    }
 }
 
 
 function deleteFriend(userId) {
-    fetch(`/accounts/api/friend/delete/${userId}/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-        }
-    })
-        .then(response => {
-            return response.json().then(data => {
-                if (!response.ok) {
-                    throw new Error(data.error);
-                }
-                return data;
+    if (confirm('Are you sure you want to delete this friend ?')) {
+        fetch(`/accounts/api/friend/delete/${userId}/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+            .then(response => {
+                return response.json().then(data => {
+                    if (!response.ok) {
+                        throw new Error(data.error);
+                    }
+                    return data;
+                });
+            })
+            .then(data => {
+                alert(data.status);
+                window.location.reload();  // ページをリロード
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert(error.message);
             });
-        })
-        .then(data => {
-            alert(data.status);
-            window.location.reload();  // ページをリロード
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert(error.message);
-        });
+
+    } else {
+        alert('Friend deletion has been canceled');
+    }
 }
