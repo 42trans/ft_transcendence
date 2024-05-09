b31786bcf83441c531e626ed39e435aa2fbf476d
hioikawa <uminomae.com>
2024-05-09 20:58:03 +0900

    [fin] tournament [bugfix] headerを突き抜けるコンテンツのバグ修正

--- docker/srcs/uwsgi-django/pong/static/pong/js/bootstrap/color-modes.js
+++ docker/srcs/uwsgi-django/pong/static/pong/js/bootstrap/color-modes.js
@@ -13,11 +13,13 @@
   const getPreferredTheme = () => {
     const storedTheme = getStoredTheme()
     if (storedTheme) {
-      return storedTheme
+      return storedTheme;
+    } else {
+      return 'dark';
     }
 
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  }
+    // return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  };
 
   const setTheme = theme => {
     if (theme === 'auto') {
