6178ac0dfb3e0e71c7b0dc264568e86b588528ec
takira <ca.fie.0220@gmail.com>
2024-05-18 10:53:47 +0900

    [update] urls: _set_i18n_url()を追加し、i18n on/offを切り替えできるように変更

--- docker/srcs/uwsgi-django/trans_pj/urls.py
+++ docker/srcs/uwsgi-django/trans_pj/urls.py
@@ -22,6 +22,14 @@ from django.conf.urls.i18n import i18n_patterns
 import accounts.urls, accounts.urls_api
 import chat.urls, chat.urls_api
 
+
+def _set_i18n_url(paths: list, enabled_i18n: bool = False):
+	if enabled_i18n:
+		return i18n_patterns(*paths, prefix_default_language=True)
+	else:
+		return paths
+
+
 urlpatterns = [
 	path("i18n/", include("django.conf.urls.i18n")),
 ]
@@ -29,28 +37,30 @@ urlpatterns = [
 # API
 urlpatterns += [
 	path('accounts/', include(accounts.urls_api)),
-	path('pong/api/', include('pong.urls_api')), 
+	path('pong/api/', include('pong.urls_api')),
 	path('chat/', include(chat.urls_api)),
 ]
 
+
 # 国際化対象のURLパターン
-urlpatterns += i18n_patterns(
-	path('pong/', include('pong.urls')),
-	path('', include('django_prometheus.urls')),
-	path('admin/', admin.site.urls),
-	path('api/status/', main_views.api_status, name='api_status'),
-	path('accounts/', include(accounts.urls)),
-	path('chat/', include(chat.urls)),
+urlpatterns += _set_i18n_url(
+	paths=[
+		path('pong/', include('pong.urls')),
+		path('', include('django_prometheus.urls')),
+		path('admin/', admin.site.urls),
+		path('api/status/', main_views.api_status, name='api_status'),
+		path('accounts/', include(accounts.urls)),
+		path('chat/', include(chat.urls)),
 
-	# テスト用　削除予定
-	path('home', main_views.index, name='home'),
-	path('lang', main_views.lang, name='lang'),
-	path('script', main_views.script, name='script'),
-	path('script2', main_views.script2, name='script2'),
+		# テスト用　削除予定
+		path('home', main_views.index, name='home'),
+		path('lang', main_views.lang, name='lang'),
+		path('script', main_views.script, name='script'),
+		path('script2', main_views.script2, name='script2'),
 
-	# SPA
-	path('', main_views.spa, name='index'),
-	# re_path(r'\w*', main_views.spa, name='index'),
-
-	prefix_default_language=True
+		# SPA
+		path('', main_views.spa, name='index'),
+		# re_path(r'\w*', main_views.spa, name='index'),
+	],
+	enabled_i18n=False
 )
