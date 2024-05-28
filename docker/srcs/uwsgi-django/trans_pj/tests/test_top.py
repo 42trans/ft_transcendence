b0b4b9376fd3944d218d50e1ca4616b087ab4ac6
takira <ca.fie.0220@gmail.com>
2024-05-28 20:12:35 +0900

    [update] test_top: link URLの評価を追加

--- docker/srcs/uwsgi-django/trans_pj/tests/test_top.py
+++ docker/srcs/uwsgi-django/trans_pj/tests/test_top.py
@@ -6,3 +6,8 @@ class TopPageTest(TestConfig):
         self._assert_page_url_and_title(expecter_url=self.top_url,
                                         expected_title='PongTop')
         # self._screenshot("top_page")
+
+        # link
+        actual_free_play_url = self._text_link_url("Free-Play")
+        expected_free_play_url = self.free_game_url
+        self.assertEqual(actual_free_play_url, expected_free_play_url)
