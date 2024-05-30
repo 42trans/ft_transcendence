e767b268402923a18b7c44128bcb5482937a3e9e
takira <ca.fie.0220@gmail.com>
2024-05-30 14:45:20 +0900

    pong: End Gameのページ遷移をSPA対応

--- docker/srcs/vite/pong-three/src/js/pongEngine/PongEngineMatch.js
+++ docker/srcs/vite/pong-three/src/js/pongEngine/PongEngineMatch.js
@@ -144,8 +144,9 @@ class PongEngineMatch
 		const button = document.createElement('button');
 		button.textContent = 'End Game';
 		button.className = 'game-button';
+		button.setAttribute('data-link', '');
 		button.onclick = function() {
-			window.location.href = '/game/';
+			window.location.href = '/app/';
 		};
 		// ボタンをページに追加
 		document.body.appendChild(button);
