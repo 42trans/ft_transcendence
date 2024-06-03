df01107b56b16fa6b970cc393c75fe1005547ec1
hioikawa <uminomae.com>
2024-06-03 22:51:27 +0900

    衝突合わせ

--- docker/srcs/uwsgi-django/pong/static/pong/js/online/PongOnlineIndex.js
+++ docker/srcs/uwsgi-django/pong/static/pong/js/online/PongOnlineIndex.js
@@ -4,8 +4,7 @@ import PongOnlineClientApp from './PongOnlineClientApp.js';
 /**
  * 2D-Pong entry point
  */
-// TODO_ft:SPA対応
-document.addEventListener('DOMContentLoaded', () => 
-{
+// document.addEventListener('DOMContentLoaded', () => 
+// {
 	PongOnlineClientApp.main();
-});
+// });
