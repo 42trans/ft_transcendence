b2592e65e6e5c729e6a35ba9471301bfab631ff1
takira <ca.fie.0220@gmail.com>
2024-06-07 23:30:54 +0900

    [update] test_url_access: login userのテストを修正

--- docker/srcs/uwsgi-django/trans_pj/tests/test_url_access.py
+++ docker/srcs/uwsgi-django/trans_pj/tests/test_url_access.py
@@ -25,7 +25,8 @@ class UrlAccessTest(TestConfig):
                               nickname=self.user2_nickname,
                               password=self.password)
 
-        self._create_new_user(email=self.user3_email,
+        self.set_up_key = self._create_new_user(
+                              email=self.user3_email,
                               nickname=self.user3_nickname,
                               password=self.password,
                               is_enable_2fa=True)
@@ -61,83 +62,86 @@ class UrlAccessTest(TestConfig):
             # 無理やり表示内容を評価
             if expected_url == self.login_url:
                 self._is_login_page()
+                print(f"           expected login: ok")
             else:
                 self._assert_current_url(expected_url)
 
-    # def test_access_by_2fa_disabled_user_(self):
-    #     """
-    #     2FA無効userでのアクセス
-    #     """
-    #     print(f"[USER: 2FA Disabled]")
-    #     self._login(email=self.user1_email, password=self.password)
-    #
-    #     for page_name, page_path in self.url_config.items():
-    #         print(f" [Testing] page_name    : {page_name}")
-    #         print(f"           page_path    : {page_path}")
-    #
-    #         if self._except_test_page(page_name):
-    #             print(f"           skip")
-    #             continue
-    #
-    #         url = self._get_url(page_name, page_path)
-    #         self._access_to(url, wait_to_be_url=False)
-    #         time.sleep(0.5)  # 明示的に待機
-    #         # self._screenshot(f"user1_{page_name}")
-    #
-    #         if self._is_page_redirect_to_top(page_name, is_enable_2fa=False):
-    #             expected_url = self.top_url
-    #         elif self._is_url_with_param(page_name):
-    #             expected_url = f"{kURL_PREFIX}{page_path}{self.user2_nickname}/"
-    #         else:
-    #             expected_url = f"{kURL_PREFIX}{page_path}"
-    #
-    #         print(f"           expected_url : {expected_url}")
-    #         print(f"           current_url  : {self.driver.current_url}")
-    #         # self._assert_current_url(expected_url)
-    #
-    #         # 無理やり表示内容を評価
-    #         if expected_url == self.top_url:
-    #             self._is_top_page()
-    #         else:
-    #             self._assert_current_url(expected_url)
-    #
-    #
-    # def test_access_by_2fa_enabled_user(self):
-    #     """
-    #     2FA有効userでのアクセス
-    #     """
-    #     print(f"[USER: 2FA Enabled]")
-    #     self._login(email=self.user1_email, password=self.password)
-    #
-    #     for page_name, page_path in self.url_config.items():
-    #         print(f" [Testing] page_name    : {page_name}")
-    #         print(f"           page_path    : {page_path}")
-    #
-    #         if self._except_test_page(page_name, page_name):
-    #             print(f"           skip")
-    #             continue
-    #
-    #         url = self._get_url(page_name, page_path)
-    #         self._access_to(url, wait_to_be_url=False)
-    #         time.sleep(0.5)  # 明示的に待機
-    #         # self._screenshot(f"user3_{page_name}")
-    #
-    #         if self._is_page_redirect_to_top(page_name, is_enable_2fa=True):
-    #             expected_url = self.top_url
-    #         elif self._is_url_with_param(page_name):
-    #             expected_url = f"{kURL_PREFIX}{page_path}{self.user2_nickname}/"
-    #         else:
-    #             expected_url = f"{kURL_PREFIX}{page_path}"
-    #
-    #         print(f"           expected_url : {expected_url}")
-    #         print(f"           current_url  : {self.driver.current_url}")
-    #         # self._assert_current_url(expected_url)
-    #
-    #         # 無理やり表示内容を評価
-    #         if expected_url == self.top_url:
-    #             self._is_top_page()
-    #         else:
-    #             self._assert_current_url(expected_url)
+    def test_access_by_2fa_disabled_user_(self):
+        """
+        2FA無効userでのアクセス
+        """
+        print(f"[USER: 2FA Disabled]")
+        self._login(email=self.user1_email, password=self.password)
+
+        for page_name, page_path in self.url_config.items():
+            print(f" [Testing] page_name    : {page_name}")
+            print(f"           page_path    : {page_path}")
+
+            if self._except_test_page(page_name):
+                print(f"           skip")
+                continue
+
+            url = self._get_url(page_name, page_path)
+            self._access_to(url, wait_to_be_url=False)
+            time.sleep(0.5)  # 明示的に待機
+            # self._screenshot(f"user1_{page_name}")
+
+            if self._is_page_redirect_to_top(page_name, is_enable_2fa=False):
+                expected_url = self.top_url
+            elif self._is_url_with_param(page_name):
+                expected_url = f"{kURL_PREFIX}{page_path}{self.user2_nickname}/"
+            else:
+                expected_url = f"{kURL_PREFIX}{page_path}"
+
+            print(f"           expected_url : {expected_url}")
+            print(f"           current_url  : {self.driver.current_url}")
+            # self._assert_current_url(expected_url)
+
+            # 無理やり表示内容を評価
+            if expected_url == self.top_url:
+                self._is_top_page()
+                print(f"           expected top : ok")
+            else:
+                self._assert_current_url(expected_url)
+
+    def test_access_by_2fa_enabled_user(self):
+        """
+        2FA有効userでのアクセス
+        """
+        print(f"[USER: 2FA Enabled]")
+        self._login(email=self.user3_email, password=self.password)
+        self._verify_login_2fa(self.set_up_key)
+
+        for page_name, page_path in self.url_config.items():
+            print(f" [Testing] page_name    : {page_name}")
+            print(f"           page_path    : {page_path}")
+
+            if self._except_test_page(page_name):
+                print(f"           skip")
+                continue
+
+            url = self._get_url(page_name, page_path)
+            self._access_to(url, wait_to_be_url=False)
+            time.sleep(0.5)  # 明示的に待機
+            # self._screenshot(f"user3_{page_name}")
+
+            if self._is_page_redirect_to_top(page_name, is_enable_2fa=True):
+                expected_url = self.top_url
+            elif self._is_url_with_param(page_name):
+                expected_url = f"{kURL_PREFIX}{page_path}{self.user2_nickname}/"
+            else:
+                expected_url = f"{kURL_PREFIX}{page_path}"
+
+            print(f"           expected_url : {expected_url}")
+            print(f"           current_url  : {self.driver.current_url}")
+            # self._assert_current_url(expected_url)
+
+            # 無理やり表示内容を評価
+            if expected_url == self.top_url:
+                self._is_top_page()
+                print(f"           expected top : ok")
+            else:
+                self._assert_current_url(expected_url)
 
     def _except_test_page(self, page_name):
         """
@@ -199,6 +203,7 @@ class UrlAccessTest(TestConfig):
         """
         'Please log in'が表示されている場合はlogin pageとみなす
         """
+        self.driver.refresh()
         h1_element = self._element(By.CSS_SELECTOR, "h1.slideup-text")
         self.assertIn("Please log in", h1_element.text)
 
@@ -206,6 +211,7 @@ class UrlAccessTest(TestConfig):
         """
         'Unrivaled hth Pong Experience'が表示されている場合は/app/とみなす
         """
+        self.driver.refresh()
         h2_element = self._element(By.CSS_SELECTOR, "h2.slideup-text.text-shadow-primary")
         self.assertIn("Unrivaled hth Pong Experience", h2_element.text)
 
