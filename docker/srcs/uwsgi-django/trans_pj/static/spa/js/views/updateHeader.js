d91cbbb06c055423be902bc8b635d743edeb398b
takira <ca.fie.0220@gmail.com>
2024-06-08 12:24:57 +0900

    [update] logout SPA化 & navi更新

--- docker/srcs/uwsgi-django/trans_pj/static/spa/js/views/updateHeader.js
+++ docker/srcs/uwsgi-django/trans_pj/static/spa/js/views/updateHeader.js
@@ -1,8 +1,13 @@
+import { setupLogoutEventListener } from "/static/accounts/js/logout.js"
+
 // headerを取得し差し替え
 export function updateHeader() {
     fetch('/spa/header/')
         .then(response => response.text())
         .then(headerHtml => {
             document.querySelector('header').innerHTML = headerHtml;
+
+            // logout buttonのイベントリスナーを設定
+            setupLogoutEventListener();
         });
 }
