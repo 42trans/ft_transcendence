dfdff272d1fda901fd6efa06ae41f0da5502ca5e
takira <ca.fie.0220@gmail.com>
2024-06-11 11:45:53 +0900

    [fix] login済みのuserの/auth/->/app/リダイレクトの際、URLが/app/に切り替わるよう修正

--- docker/srcs/uwsgi-django/trans_pj/static/spa/js/index.js
+++ docker/srcs/uwsgi-django/trans_pj/static/spa/js/index.js
@@ -2,6 +2,7 @@
 
 import { routeTable } from "./routing/routeTable.js"
 import { switchPage, renderView } from "./routing/renderView.js";
+import { isUserLoggedIn, isUserEnable2FA } from "./utility/isUser.js"
 import { setOnlineStatus } from "/static/accounts/js/online-status.js";
 
 
@@ -49,9 +50,39 @@ const setupDOMContentLoadedListener = () => {
 };
 
 
+// login userであれば/auth/への遷移を/app/に切り返る
+async function getLoggedInUserRedirectUrl(url) {
+  const isLoggedIn = await isUserLoggedIn();
+  if (!isLoggedIn) {
+    return url;
+  }
+  // 2FA有効user && enable2faへの遷移 は/app/に切り替える
+  const isEnable2FA = await isUserEnable2FA();
+
+  const urlObject = new URL(url);
+  const pathName = urlObject.pathname;
+  let nextUrl;
+  if (pathName === routeTable['signup'].path
+      || pathName === routeTable['login'].path
+      || pathName === routeTable['veryfy2fa'].path
+      || (pathName === routeTable['enable2fa'].path && isEnable2FA)) {
+    nextUrl = new URL(routeTable['top'].path, window.location.origin);
+  } else {
+    nextUrl = url;
+  }
+
+  // console.log(`DEBUG getLoggedInUserRedirectUrl`)
+  // console.log(` url      :${url}`)
+  // console.log(` pathName :${pathName}`)
+  // console.log(` nextUrl  :${nextUrl}`)
+  // alert(`[check console log]getLoggedInUserRedirectUrl`)
+  return nextUrl;
+}
+
+
 // リンクのクリックイベントで発火
 const setupBodyClickListener = () => {
-  document.body.addEventListener("click", (event) => {
+  document.body.addEventListener("click", async (event) => {
   console.log('clickEvent: path: ' + window.location.pathname);
   stopGamePageAnimation()
 
@@ -59,7 +90,9 @@ const setupBodyClickListener = () => {
     if (linkElement) {
       console.log('clickEvent: taga-link');
       event.preventDefault();
-      const url = linkElement.href;
+
+      const linkUrl = linkElement.href;
+      const url = await getLoggedInUserRedirectUrl(linkUrl)
       switchPage(url);
     }
 
