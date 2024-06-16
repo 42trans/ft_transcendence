3f9699d990d59e721b543dc255e2ac7532848bb1
hioikawa <uminomae.com>
2024-06-16 16:47:02 +0900

    pongEngine

--- docker/srcs/vite/pong-three/src/js/pongEngine/PongEngine.js
+++ docker/srcs/vite/pong-three/src/js/pongEngine/PongEngine.js
@@ -8,6 +8,12 @@ import PongEngineMatch from './PongEngineMatch';
 import RendererManager from '../manager/RendererManager';
 import PongEngineInit from "./PongEngineInit";
 import AllScenesManager from '../manager/AllScenesManager';
+import { handleCatchError } from '../../index.js';
+
+
+const DEBUG_FLOW 		= 0;
+const TEST_TRY1			= 0;
+const TEST_TRY2			= 0;
 
 /**
  * 参考:【nklsrh/BuildNewGames_ThreeJSGame: A game built to show off some of the basic features of the Three.JS WebGL library.】 <https://github.com/nklsrh/BuildNewGames_ThreeJSGame/tree/gh-pages>
@@ -16,18 +22,22 @@ class PongEngine
 {
 	constructor(PongApp) 
 	{
-		this.pongApp	= PongApp
-		this.env		= PongApp.env;
-		this.matchData	= PongApp.matchData;
-		
-		this.init3DEnvironment();
-		this.initParameters();
-		this.initGameLogic();
-		
+		try {
+			this.pongApp	= PongApp
+			this.env		= PongApp.env;
+			this.matchData	= PongApp.matchData;
+			
+			this.init3DEnvironment();
+			this.initParameters();
+			this.initGameLogic();
 
-		this.isRunning	= true;
-		this.animate	= this.animate.bind(this);
-		setTimeout(() => this.animate(), 4500);
+			this.isRunning	= true;
+			this.animate	= this.animate.bind(this);
+			setTimeout(() => this.animate(), 4500);
+		} catch (error) {
+			console.error('hth: GameplayState.enter() failed', error);
+			handleCatchError(error);
+		}
 	}
 
 	init3DEnvironment() 
@@ -50,19 +60,26 @@ class PongEngine
 		this.physics	= new PongEnginePhysics(this.data);
 		this.match		= new PongEngineMatch(this.pongApp, this, this.scene, this.data);
 		this.update		= new PongEngineUpdate(this, this.data, this.physics, this.match);
+					if (TEST_TRY1) {	throw new Error('TEST_TRY1');	}
 	}
 
 	async animate() 
 	{
-		if (this.isRunning)
-		{
-			requestAnimationFrame(this.animate);
-			// ここで描画ループを止めると終了後のアスペクト比がバグる
-		// } else {
-		// 	// ゲームが終了した場合は、描画ループを停止
-		// 	this.pongApp.stopRenderLoop();
+		try {
+			if (this.isRunning)
+			{
+				requestAnimationFrame(this.animate);
+				// ここで描画ループを止めると終了後のアスペクト比がバグる
+			// } else {
+			// 	// ゲームが終了した場合は、描画ループを停止
+			// 	this.pongApp.stopRenderLoop();
+			}
+			await this.update.updateGame();
+			if (TEST_TRY2) {	throw new Error('TEST_TRY2');	}
+		} catch (error) {
+			console.error('hth: GameplayState.enter() failed', error);
+			// handleCatchError(error);
 		}
-		await this.update.updateGame();
 	}
 }
 
