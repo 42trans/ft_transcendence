f14be337f63919355ca6984cf1e278e31ff70a78
hioikawa <uminomae.com>
2024-06-17 05:44:55 +0900

    unused file

--- docker/srcs/uwsgi-django/trans_pj/static/spa/js/views/pong/FreePlay.js
+++ docker/srcs/uwsgi-django/trans_pj/static/spa/js/views/pong/FreePlay.js
@@ -1,24 +1,24 @@
-import AbstractView from "../AbstractView.js";
-import fetchData from "../../utility/fetch.js";
-import { getUrl } from "../../utility/url.js";
-import { loadAndExecuteScript } from "../../utility/script.js";
+// import AbstractView from "../AbstractView.js";
+// import fetchData from "../../utility/fetch.js";
+// import { getUrl } from "../../utility/url.js";
+// import { loadAndExecuteScript } from "../../utility/script.js";
 
 
-export default class extends AbstractView {
-  constructor(params) {
-    super(params);
-    this.setTitle("FreePlay");
-  }
+// export default class extends AbstractView {
+//   constructor(params) {
+//     super(params);
+//     this.setTitle("FreePlay");
+//   }
 
-  async getHtml() {
-    const uri = "/pong/game/";
-    const data = await fetchData(uri);
-    //console.log("Pong:" + data);
-    return data;
-  }
+//   async getHtml() {
+//     const uri = "/pong/game/";
+//     const data = await fetchData(uri);
+//     //console.log("Pong:" + data);
+//     return data;
+//   }
 
-  async executeScript(spaElement) {
-    loadAndExecuteScript(spaElement, "/static/pong/three/assets/index.js", true);
-  }
+//   async executeScript(spaElement) {
+//     loadAndExecuteScript(spaElement, "/static/pong/three/assets/index.js", true);
+//   }
 
-}
+// }
