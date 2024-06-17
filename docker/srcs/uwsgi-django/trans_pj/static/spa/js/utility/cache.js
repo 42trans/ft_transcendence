d9cacdb1e15cd4b2a878eb26eada048d8b3b0107
takira <ca.fie.0220@gmail.com>
2024-06-18 00:36:06 +0900

    [add] cache管理用の関数を追加

---
+++ docker/srcs/uwsgi-django/trans_pj/static/spa/js/utility/cache.js
@@ -0,0 +1,24 @@
+// static/spa/js/utility/cache.js
+
+const DEBUG_LOG = 1;
+
+export function setNoCache() {
+    if (DEBUG_LOG) { console.log("set no cache"); }
+
+    // IE
+    if (!window.onbeforeunload) {
+        window.onbeforeunload = function() {};
+    }
+
+    // IE以外
+    if (!window.onload) {
+        window.onunload = function() {};
+    }
+}
+
+export function clearNoCache() {
+    if (DEBUG_LOG) { console.log("no cache clear"); }
+
+    window.onbeforeunload = null;
+    window.onunload = null;
+}
