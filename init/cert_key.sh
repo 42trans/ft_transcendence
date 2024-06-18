219bcd61fb24a20646d3cd43dcd8cfe4b36c6f5e
hioikawa <uminomae.com>
2024-06-18 16:10:41 +0900

    crt,keyをssl/へcp

--- init/cert_key.sh
+++ init/cert_key.sh
@@ -10,10 +10,11 @@ RESET="\033[0m"
 EXIT_FAILURE=1
 
 SSL_DIR=./docker/srcs/nginx/ssl
-
-# 同じ証明書を nginx-Django-postgre間の通信に用いる場合
-# DJANGO_SSL_DIR=./docker/srcs/uwsgi-django/ssl
-# POSTGRES_SSL_DIR=./docker/srcs/postgres/ssl
+# ---------------------------------------------------
+# 同じ証明書を nginx-Django-postgre間の通信に用いる場合　下記を追加
+DJANGO_SSL_DIR=./docker/srcs/uwsgi-django/ssl
+POSTGRES_SSL_DIR=./docker/srcs/postgres/ssl
+# ---------------------------------------------------
 
 if [ ! -f $SSL_DIR/openssl.cnf ]; then
     echo -e "${RED}[Error] openssl.cnf not found${RESET}"
@@ -24,8 +25,11 @@ if [ ! -f $SSL_DIR/nginx.key ] || [ ! -f $SSL_DIR/nginx.crt ]; then
   echo "generate cert_key"
 
   mkdir -p $SSL_DIR/
-
-  # mkdir -p $DJANGO_SSL_DIR/
+# ---------------------------------------------------
+# 同じ証明書を nginx-Django-postgre間の通信に用いる場合 下記を追加
+  mkdir -p $DJANGO_SSL_DIR/
+  mkdir -p $POSTGRES_SSL_DIR/
+# ---------------------------------------------------
 
   openssl req \
           -new \
@@ -35,11 +39,15 @@ if [ ! -f $SSL_DIR/nginx.key ] || [ ! -f $SSL_DIR/nginx.crt ]; then
           -config $SSL_DIR/openssl.cnf \
           -keyout $SSL_DIR/nginx.key \
           -out $SSL_DIR/nginx.crt > /dev/null 2>&1
-  
-  # cp $SSL_DIR/nginx.key $DJANGO_SSL_DIR/django.key
-  # cp $SSL_DIR/nginx.crt $DJANGO_SSL_DIR/django.crt
-  # cp $SSL_DIR/nginx.key $POSTGRES_SSL_DIR/server.key
-  # cp $SSL_DIR/nginx.crt $POSTGRES_SSL_DIR/server.crt
+
+# ---------------------------------------------------
+# 同じ証明書を nginx-Django-postgre間の通信に用いる場合 下記を追加
+# ---------------------------------------------------
+  cp $SSL_DIR/nginx.key $DJANGO_SSL_DIR/django.key
+  cp $SSL_DIR/nginx.crt $DJANGO_SSL_DIR/django.crt
+  cp $SSL_DIR/nginx.key $POSTGRES_SSL_DIR/server.key
+  cp $SSL_DIR/nginx.crt $POSTGRES_SSL_DIR/server.crt
+# ---------------------------------------------------
 
 else
   echo "cert_key already exists"
