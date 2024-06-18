b502189ac3053262e241a7d0ccd73c8228035473
takira <ca.fie.0220@gmail.com>
2024-06-18 13:57:31 +0900

    fix: dm, play_tournamentのパラメータをstrに変更し、URLに文字列が入力された時のハンドリングを追加

--- docker/srcs/uwsgi-django/chat/views/views.py
+++ docker/srcs/uwsgi-django/chat/views/views.py
@@ -75,9 +75,17 @@ class DMView(LoginRequiredMixin, TemplateView):
     Login認証していなければ accounts:login へ遷移
     """
     template_name = "chat/dm_with.html"
-    error_occurred_redirect_to = "chat:dm_sessions"
+    error_occurred_redirect_to = "/pong/"
 
     def get(self, request, target_id: str):
+        # target_idが整数でない場合の遷移
+        try:
+            target_id = int(target_id)
+            if target_id <= 0:
+                return redirect(self.error_occurred_redirect_to)
+        except ValueError:
+            return redirect(self.error_occurred_redirect_to)
+
         # user, other_userを取得
         # dm targetのnicknameがuser.nicknameである場合はerr
         user, dm_target, err = _get_dm_users(request, target_id)
