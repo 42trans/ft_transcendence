8f6454d88b7a3cfa2dd01ec09661945c88fad17d
takira <ca.fie.0220@gmail.com>
2024-06-19 14:59:17 +0900

    [add] cert key作成

--- init/cert_key_django.sh
+++ init/cert_key_django.sh
@@ -9,15 +9,15 @@ RED="\033[0;31m"
 RESET="\033[0m"
 EXIT_FAILURE=1
 
-SSL_DIR=./docker/srcs/nginx/ssl
+SSL_DIR=./docker/srcs/uwsgi-django/ssl
 
 if [ ! -f $SSL_DIR/openssl.cnf ]; then
     echo -e "${RED}[Error] openssl.cnf not found${RESET}"
     exit EXIT_FAILURE
 fi
 
-if [ ! -f $SSL_DIR/nginx.key ] || [ ! -f $SSL_DIR/nginx.crt ]; then
-  echo "generate nginx cert_key"
+if [ ! -f $SSL_DIR/django.key ] || [ ! -f $SSL_DIR/django.crt ]; then
+  echo "generate django cert_key"
 
   mkdir -p $SSL_DIR/
 
@@ -27,8 +27,8 @@ if [ ! -f $SSL_DIR/nginx.key ] || [ ! -f $SSL_DIR/nginx.crt ]; then
           -nodes \
           -days 365 \
           -config $SSL_DIR/openssl.cnf \
-          -keyout $SSL_DIR/nginx.key \
-          -out $SSL_DIR/nginx.crt > /dev/null 2>&1
+          -keyout $SSL_DIR/django.key \
+          -out $SSL_DIR/django.crt > /dev/null 2>&1
 else
-  echo "nginx cert_key already exists"
+  echo "django cert_key already exists"
 fi
