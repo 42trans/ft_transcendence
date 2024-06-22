9ee7dff1231d4a081170bdff9e1b69933e3d517b
takira <ca.fie.0220@gmail.com>
2024-06-23 08:52:38 +0900

    update: release_matchをIsAuthenticatedに変更

--- docker/srcs/uwsgi-django/pong/views/tournament/save_views.py
+++ docker/srcs/uwsgi-django/pong/views/tournament/save_views.py
@@ -17,12 +17,14 @@ logger = logging.getLogger('django')
 User = get_user_model()
 
 
-@require_POST
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def release_match(request, match_id):
 	match = get_object_or_404(Match, id=match_id)
 	match.is_playing = False
 	match.save()
 	return JsonResponse({'status': 'ok'})
+
 
 def assign_winner_to_next_match(current_match: Match, winner_nickname: str):
 	def __is_valid_argument(current_match: Match,
