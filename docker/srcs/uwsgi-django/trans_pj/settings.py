9bf62cbc2b5ae9776e605cc0d74780d47dd2748e
takira <ca.fie.0220@gmail.com>
2024-06-22 10:32:16 +0900

    fix: _load_url_config path

--- docker/srcs/uwsgi-django/trans_pj/settings.py
+++ docker/srcs/uwsgi-django/trans_pj/settings.py
@@ -366,10 +366,14 @@ CHANNEL_LAYERS = {
 
 def _load_url_config():
 	try:
-		file_path = ('static/spa/json/urlConfig.json')
+		base_dir = os.path.dirname(os.path.abspath(__file__))
+		file_path = os.path.join(base_dir, 'static', 'spa', 'json', 'urlConfig.json')
+		print(f"base_dir: {base_dir}")
+
+		# file_path = ('static/spa/json/urlConfig.json')
 		with open(file_path) as f:
 			url_config = json.load(f)
-			# print(f'load_url_config: {url_config}')
+			print(f'load_url_config: {url_config}')
 			return url_config
 	except Exception as e:
 		print(f'load_url_config: Error: could not load urlConfig: {str(e)}')
