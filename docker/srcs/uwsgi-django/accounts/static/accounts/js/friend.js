2a38a088ab842ab9d08f6734780aaf7f0b692bf7
hioikawa <uminomae.com>
2024-06-23 19:14:26 +0900

    slidup-textアニメーション適用：衝突箇所、新規UIに合わせて微調整箇所など

--- docker/srcs/uwsgi-django/accounts/static/accounts/js/friend.js
+++ docker/srcs/uwsgi-django/accounts/static/accounts/js/friend.js
@@ -275,7 +275,7 @@ export function createActionButton(text, action) {
     const actionLink = document.createElement('a');
     actionLink.textContent = text;
     actionLink.href = "#";
-    actionLink.className = text.toLowerCase() + 'Button' + ' hth-btn my-4';
+    actionLink.className = text.toLowerCase() + 'Button' + ' hth-btn my-4 mx-4';
     actionLink.onclick = function(event) {
         event.preventDefault();
         action();
